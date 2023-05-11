db8820b90cf43228f67a7178c933cc127ac1813d
Vladislav <vladislav.korbakov@yandex.ru>
2023-05-11 21:57:29 +0300

    Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт,
+//  является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите любое натуральное число ");
+int number = int.Parse(Console.ReadLine());
+int remain = number % 2;
+if (remain == 0)
+{
+    Console.WriteLine("Число " + number + " четное?");
+    Console.WriteLine("ДА!");
+}
+else
+{
+    Console.WriteLine("Число " + number + " четное?");
+    Console.WriteLine("НЕТ!");
+}
