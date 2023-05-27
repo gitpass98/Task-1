eceffe65c1ef884c0f15259f9f17c0d9a51a0e41
Vladislav <vladislav.korbakov@yandex.ru>
2023-05-27 14:55:09 +0300

    Task 09

---
+++ Task09/Program.cs
@@ -0,0 +1,40 @@
+﻿// 9. Напишите программу, которая 
+// 1. выводит случайное число 
+//  2. из отрезка [10, 99] и 
+// 3. показывает наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+
+int number = new Random().Next(10, 100); // 10, 99 + 1 
+Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
+
+
+ int firstDigit = number / 10;
+ int secondDigit = number % 10;
+
+// if(firstDigit > secondDigit)
+// {
+//     Console.WriteLine($"Наибольшая цифра  числа -> {firstDigit}");
+// }
+// else
+// {
+//    Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}"); 
+// }
+
+// Тернарный операвтор
+//  int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int MaxDigit (int num) // num = number
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return maxDigit;
+}
---
+++ Task11/Program.cs
@@ -0,0 +1,22 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+
+
+int number = new Random().Next(100, 1000); // 10, 999 + 1 
+Console.WriteLine($"Случайное число из отрезка 100 - 999 -> {number}");
+
+int DeleteSecondDigit (int num)
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    int newDigit = firstDigit	* 10 + thirdDigit;
+    return newDigit;
+}
+
+int result = DeleteSecondDigit(number);
+Console.WriteLine($"{number} -> {result}");
